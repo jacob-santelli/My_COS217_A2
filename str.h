f791f2103bc66362d3fc9db672de3e8adb9a59c5
santelli <santelli@princeton.edu>
2022-10-04 17:56:54 -0400

    here we go lol

---
+++ str.h
@@ -0,0 +1,5 @@
+#ifndef STR_INCLUDED
+#define STR_INCLUDED
+size_t Str_getLength(const char pcSrc[]);
+int lcm(int i, int j);
+#endif
---
+++ stra.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+size_t Str_getLength(const char pcSrc[])
+{
+   size_t uLength = 0;
+   assert(pcSrc != NULL);
+   while (pcSrc[uLength] != '\0')
+      uLength++;
+   return uLength;
+}
